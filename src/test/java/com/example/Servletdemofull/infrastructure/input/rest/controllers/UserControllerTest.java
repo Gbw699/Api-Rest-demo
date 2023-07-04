565079b04731e6ce855f9434f492701df1d7da93
GabyBarimboim <gdbarimboim@gmail.com>
2023-07-04 14:38:06 -0300

    updateUserTest ready

--- src/test/java/com/example/Servletdemofull/infrastructure/input/rest/controllers/UserControllerTest.java
+++ src/test/java/com/example/Servletdemofull/infrastructure/input/rest/controllers/UserControllerTest.java
@@ -1,23 +1,16 @@
 package com.example.Servletdemofull.infrastructure.input.rest.controllers;
 
-import com.example.Servletdemofull.infrastructure.input.rest.dtos.UserDto;
 import com.example.Servletdemofull.infrastructure.input.rest.mappers.UserMapper;
 import com.example.Servletdemofull.infrastructure.output.entity.User;
 import com.example.Servletdemofull.infrastructure.output.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -25,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.*;
 
 import static org.mockito.Mockito.when;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @WebMvcTest(UserController.class)
@@ -98,7 +92,20 @@ class UserControllerTest {
     }
 
     @Test
-    void updateUser() {
+    void updateUser() throws Exception {
+        //Given
+        UUID id = UUID.randomUUID();
+        User user = new User(id, "Juan", "Perez", "grg@gmail.com", "+542616320489");
+
+        //expectation (se utliza cuando se espera algo de la BDD)
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+//        when(userRepository.save(user)).thenReturn(null);
+
+        //perform
+        mockMvc.perform(put("/api/user/{id}", id).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(user)))
+                .andExpect(status().isOk())
+                .andDo(print());
     }
 
     @Test
