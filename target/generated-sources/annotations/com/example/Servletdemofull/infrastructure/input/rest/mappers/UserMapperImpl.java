f0068ae1b43526cf9da7332cd989c17d038dd0f5
GabyBarimboim <gdbarimboim@gmail.com>
2023-07-05 11:58:02 -0300

    spring boot security initial configuration

--- target/generated-sources/annotations/com/example/Servletdemofull/infrastructure/input/rest/mappers/UserMapperImpl.java
+++ target/generated-sources/annotations/com/example/Servletdemofull/infrastructure/input/rest/mappers/UserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-07-04T09:55:37-0300",
+    date = "2023-07-05T10:40:31-0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 20.0.1 (Oracle Corporation)"
 )
 @Component
