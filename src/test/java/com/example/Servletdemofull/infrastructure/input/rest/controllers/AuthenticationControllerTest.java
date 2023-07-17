56d432d9bb969f98ab02a23ae788a33864081093
GabyBarimboim <gdbarimboim@gmail.com>
2023-07-17 11:33:40 -0300

    fix AuthenticationControllerTest authenticationTest

--- src/test/java/com/example/Servletdemofull/infrastructure/input/rest/controllers/AuthenticationControllerTest.java
+++ src/test/java/com/example/Servletdemofull/infrastructure/input/rest/controllers/AuthenticationControllerTest.java
@@ -1,47 +1,34 @@
 package com.example.Servletdemofull.infrastructure.input.rest.controllers;
 
-import com.example.Servletdemofull.infrastructure.input.rest.dtos.AuthenticationRequestDto;
 import com.example.Servletdemofull.infrastructure.input.rest.dtos.RegisterRequestDto;
-import com.example.Servletdemofull.infrastructure.output.repository.UserRepository;
+import com.example.Servletdemofull.infrastructure.input.rest.dtos.AuthenticationRequestDto;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.springframework.http.MediaType;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class AuthenticationControllerTest {
 
-    @MockBean
-    private UserRepository userRepository;
-
-    @MockBean
-    private UserDetailsService userDetailsService;
-
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper objectMapper;
-
-//   @Autowired
-//   private AuthenticationManager authenticationManager;
 
     @Test
     void registerTest() throws Exception {
@@ -57,20 +44,12 @@ class AuthenticationControllerTest {
                 .andDo(print());
     }
 
-    @Disabled
     @Test
     void authenticationTest() throws Exception {
         //given
         AuthenticationRequestDto requestDto = new AuthenticationRequestDto("jcarlos@gmail.com", "admin");
-//        authenticationManager.authenticate(
-//                new UsernamePasswordAuthenticationToken(
-//                        "jcarlos@gmail.com",
-//                        "admin"
-//                )
-//        );
 
         //expectation
-//        when(userRepository.findByEmail(requestDto.getEmail())).thenReturn(user);
 
         //performs
         mockMvc.perform(post("/api/v1/auth/authentication").contentType(MediaType.APPLICATION_JSON)
