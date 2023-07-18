3bbf3ea1996387a317e4c198e6669167489eb398
GabyBarimboim <gdbarimboim@gmail.com>
2023-07-18 14:23:52 -0300

    order imports from GetAllPatientsServiceTest

--- src/test/java/com/example/Servletdemofull/application/services/GetAllPatientsServiceTest.java
+++ src/test/java/com/example/Servletdemofull/application/services/GetAllPatientsServiceTest.java
@@ -2,17 +2,17 @@ package com.example.Servletdemofull.application.services;
 
 import com.example.Servletdemofull.infrastructure.output.entity.Patient;
 import com.example.Servletdemofull.infrastructure.output.repository.PatientRepository;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class GetAllPatientsServiceTest {
