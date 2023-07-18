e4f401f3ba000c8f23861a5eaa5d2f707b6f6de0
GabyBarimboim <gdbarimboim@gmail.com>
2023-07-18 14:22:03 -0300

    create getTest from GetAllPatientsServiceTest

---
+++ src/test/java/com/example/Servletdemofull/application/services/GetAllPatientsServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.Servletdemofull.application.services;
+
+import com.example.Servletdemofull.infrastructure.output.entity.Patient;
+import com.example.Servletdemofull.infrastructure.output.repository.PatientRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class GetAllPatientsServiceTest {
+
+    GetAllPatientsService getAllPatientsService;
+
+    @Mock
+    private PatientRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        getAllPatientsService = new GetAllPatientsService(repository);
+    }
+
+    @Test
+    void getTest() {
+
+        Optional<List<Patient>> patients = Optional.of(repository.findAll());
+
+        Optional<List<Patient>> result = getAllPatientsService.get();
+
+        Assertions.assertEquals(patients, result);
+    }
+}
