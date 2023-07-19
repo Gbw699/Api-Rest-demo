e011960aad9bcef0a811d3286a4141dad967a989
GabyBarimboim <gdbarimboim@gmail.com>
2023-07-19 10:11:32 -0300

    create test for delete patient by id service

--- src/main/java/com/example/Servletdemofull/application/services/DeletePatientByIdService.java
+++ src/main/java/com/example/Servletdemofull/application/services/DeletePatientByIdService.java
@@ -1,8 +1,10 @@
 package com.example.Servletdemofull.application.services;
 
 import com.example.Servletdemofull.infrastructure.output.repository.PatientRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
---
+++ src/test/java/com/example/Servletdemofull/application/services/DeletePatientByIdServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.Servletdemofull.application.services;
+
+import com.example.Servletdemofull.infrastructure.output.repository.PatientRepository;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+class DeletePatientByIdServiceTest {
+
+    DeletePatientByIdService deletePatientByIdService;
+    @Mock
+    private PatientRepository repository;
+
+    @BeforeEach
+    void setUp() {
+        deletePatientByIdService = new DeletePatientByIdService(repository);
+    }
+
+    @Test
+    void deleteByIdTest() {
+        String expect = "El paciente se borró exitosamente de la BDD";
+
+        String actual = deletePatientByIdService.deleteById(UUID.randomUUID());
+
+        Assertions.assertEquals(expect, actual);
+    }
+}
